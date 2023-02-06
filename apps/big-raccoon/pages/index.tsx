08b84fd5706498183ab3521b7ceb80b8e4050d4d
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2023-02-06 16:30:50 +0000

    Update index.tsx

--- apps/big-raccoon/pages/index.tsx
+++ apps/big-raccoon/pages/index.tsx
@@ -1,8 +1,11 @@
+import { TopicButton } from '@nordic-bandits/ui';
+
 export function Index() {
   return (
     <div className="p-3">
       <div className="bg-red-300 prose">
         <h1>Welcome big-raccoon</h1>
+        <TopicButton />
       </div>
     </div>
   );
